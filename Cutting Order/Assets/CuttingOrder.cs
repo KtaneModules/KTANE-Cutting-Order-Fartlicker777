344a839b6b9fd18dc1afcdfe91bc51d848f4c45d
Apolo Simon <62307787+Fartlicker777@users.noreply.github.com>
2024-06-30 03:58:22 -0500

    Made model kinda

---
+++ Cutting Order/Assets/CuttingOrder.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+using Math = ExMath;
+
+public class CuttingOrder : MonoBehaviour {
+
+   public KMBombInfo Bomb;
+   public KMAudio Audio;
+
+   public KMSelectable[] Wires;
+
+   static int ModuleIdCounter = 1;
+   int ModuleId;
+   private bool ModuleSolved;
+
+   void Awake () { //Avoid doing calculations in here regarding edgework. Just use this for setting up buttons for simplicity.
+      ModuleId = ModuleIdCounter++;
+      GetComponent<KMBombModule>().OnActivate += Activate;
+      
+      foreach (KMSelectable Wire in Wires) {
+          Wire.OnInteract += delegate () { WireCut(Wire); return false; };
+      }
+      
+
+      //button.OnInteract += delegate () { buttonPress(); return false; };
+
+   }
+
+   void WireCut (KMSelectable Wire) {
+      Debug.Log("w");
+   }
+
+   void OnDestroy () { //Shit you need to do when the bomb ends
+      
+   }
+
+   void Activate () { //Shit that should happen when the bomb arrives (factory)/Lights turn on
+
+   }
+
+   void Start () { //Shit that you calculate, usually a majority if not all of the module
+      
+   }
+
+   void Update () { //Shit that happens at any point after initialization
+
+   }
+
+   void Solve () {
+      GetComponent<KMBombModule>().HandlePass();
+   }
+
+   void Strike () {
+      GetComponent<KMBombModule>().HandleStrike();
+   }
+
+#pragma warning disable 414
+   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+   IEnumerator ProcessTwitchCommand (string Command) {
+      yield return null;
+   }
+
+   IEnumerator TwitchHandleForcedSolve () {
+      yield return null;
+   }
+}
